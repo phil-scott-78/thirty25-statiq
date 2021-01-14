80ab23fd4ffcb13cebf90b1cfd0c2ad88fcc728e
Phil Scott <phil.scott@gmail.com>
2021-01-14 13:50:44 -0500

    adding more meta data

--- Constants.cs
+++ Constants.cs
@@ -3,6 +3,7 @@
     public class Constants
     {
         public const string BlogTitle = "Thirty25";
+        public const string Description = "A collection of my bad ideas and questionable architectural decisions.";
         public const int PostsPerPage = 10;
     }
 }
