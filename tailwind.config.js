d32a05f77164d867856482d3427fe0b081aa3dad
Phil Scott <phil.scott@gmail.com>
2021-01-14 14:40:37 -0500

    changing purge path to public

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./output/**/*.html"],
+  purge: ["./public/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
