a8a4e7572eaaaf96e7e66ba80ed290fc5a977ddf
Phil Scott <phil.scott@gmail.com>
2021-01-18 16:43:39 -0500

    moving to C# 9 and fixing inherits statements for rider

--- Program.cs
+++ Program.cs
@@ -1,19 +1,6 @@
-﻿using System.Threading.Tasks;
-using Statiq.App;
-using Statiq.Highlight;
+﻿using Statiq.App;
 using Statiq.Web;
 
-namespace Thirty25
-{
-    class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var highlight = new HighlightCode();
-
-            await Bootstrapper.Factory
-                .CreateWeb(args)
-                .RunAsync();
-        }
-    }
-}
+await Bootstrapper.Factory
+    .CreateWeb(args)
+    .RunAsync();
