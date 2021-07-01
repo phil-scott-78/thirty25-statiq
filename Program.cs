5872f3557f77ce2549a0e0fbd75c36ee3f8dcc1e
Phil Scott <phil.scott@gmail.com>
2021-06-30 20:38:20 -0400

    trying install script (#6)

--- Program.cs
+++ Program.cs
@@ -1,21 +1,11 @@
-﻿using System.Threading.Tasks;
-using Statiq.App;
+﻿using Statiq.App;
 using Statiq.Common;
 using Statiq.Web;
 
-namespace Thirty25
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            await Bootstrapper.Factory
-                .CreateWeb(args)
-                .AddProcess(ProcessTiming.Initialization,
-                    _ => new ProcessLauncher("npm", "install") { LogErrors = false })
-                .AddProcess(ProcessTiming.AfterExecution,
-                    _ => new ProcessLauncher("npm", "run", "build:tailwind") { LogErrors = false, })
-                .RunAsync();
-        }
-    }
-}
+await Bootstrapper.Factory
+    .CreateWeb(args)
+    .AddProcess(ProcessTiming.Initialization,
+        _ => new ProcessLauncher("npm", "install") { LogErrors = false })
+    .AddProcess(ProcessTiming.AfterExecution,
+        _ => new ProcessLauncher("npm", "run", "build:tailwind") { LogErrors = false, })
+    .RunAsync();
