ea6ea6295e19572c4e39aa0c89a3594efee589c7
Phil Scott <phil.scott@gmail.com>
2021-07-06 23:47:58 -0400

    forcing url to thirty25.com for social cards

--- Constants.cs
+++ Constants.cs
@@ -5,6 +5,7 @@
         public const string BlogTitle = "Thirty25";
         public const string Description = "A collection of my bad ideas and questionable architectural decisions.";
         public const string Creator = "@Philco78";
+        public const string ProdSiteUrl = "https://thirty25.com/";
         public const int PostsPerPage = 10;
     }
 }
