d097bb353d526f60131a0e76a2ce40b8016e2d7c
Phil Scott <phil.scott@gmail.com>
2022-01-05 12:55:05 -0500

    reworking to make playwright seemless

--- Program.cs
+++ Program.cs
@@ -2,11 +2,18 @@
 using Statiq.Common;
 using Statiq.Web;
 
+var dotnetPath = System.Environment.GetEnvironmentVariable("DOTNET_PATH") ?? "dotnet";
+
 await Bootstrapper.Factory
     .CreateWeb(args)
+    .SetOutputPath("public")
     .AddShortcode<FullUrlShortCode>("FullUrl")
     .AddProcess(ProcessTiming.Initialization,
         _ => new ProcessLauncher("npm", "install") { LogErrors = false })
+    .AddProcess(ProcessTiming.Initialization,
+        _ => new ProcessLauncher(dotnetPath, "tool restore") { LogErrors = false })
+    .AddProcess(ProcessTiming.Initialization,
+        _ => new ProcessLauncher(dotnetPath, "tool run playwright install chromium") { LogErrors = false })
     .AddProcess(ProcessTiming.AfterExecution,
         _ => new ProcessLauncher("npm", "run", "build:tailwind") { LogErrors = false, })
     .RunAsync();
