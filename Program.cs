a0b4996c553873f1d1347d3cdc4b21802ebb9fe8
Phil Scott <phil.scott@gmail.com>
2022-04-08 23:28:48 -0400

    removing tailwind and adding monorail

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Statiq.Common;
 using Statiq.Feeds;
 using Statiq.Web;
 using Statiq.Web.Pipelines;
+using Thirty25;
 using Thirty25.Statiq.Helpers;
 
 [assembly: AspMvcPartialViewLocationFormat("~/input/{0}.cshtml")]
@@ -19,16 +20,13 @@ await Bootstrapper.Factory
     .AddSetting(FeedKeys.Copyright, DateTime.UtcNow.Year.ToString())
     .SetOutputPath("public")
     .AddShortcode<FullUrlShortCode>("FullUrl")
+    .AddSetting(Constants.CssFile, "assets/styles.css")
     .ModifyPipeline(nameof(Content), pipeline =>
     {
         pipeline.PostProcessModules.Add(new RoslynHighlightModule());
     })
     .AddProcess(ProcessTiming.Initialization,
-        _ => new ProcessLauncher("npm", "install") { LogErrors = false })
-    .AddProcess(ProcessTiming.Initialization,
         _ => new ProcessLauncher(dotnetPath, "tool restore") { LogErrors = false })
     .AddProcess(ProcessTiming.Initialization,
         _ => new ProcessLauncher(dotnetPath, "tool run playwright install chromium") { LogErrors = false })
-    .AddProcess(ProcessTiming.AfterExecution,
-        _ => new ProcessLauncher("npm", "run", "build:tailwind") { LogErrors = false, })
     .RunAsync();
